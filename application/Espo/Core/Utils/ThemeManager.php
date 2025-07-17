1defb60b78428158f72154df93099f18b2418cae
Emir Cica <emircica.ec@gmail.com>
2025-07-17 12:50:47 +0300

    Logo son deneme

--- application/Espo/Core/Utils/ThemeManager.php
+++ application/Espo/Core/Utils/ThemeManager.php
@@ -35,7 +35,7 @@ class ThemeManager
 {
     private string $defaultName = 'Espo';
 
-    private string $defaultLogoSrc = 'client/img/zerosoft_logo_blue.svg';
+    private string $defaultLogoSrc = 'client/img/zerosoftbluelogo.svg';
 
     public function __construct(
         private Config $config,
