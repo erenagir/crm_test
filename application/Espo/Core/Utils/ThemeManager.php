dad4f3160535d497a83a8d52b2cda568a95e8bc9
Emir Cica <emircica.ec@gmail.com>
2025-07-17 09:54:22 +0300

    Logo değiştirildi

--- application/Espo/Core/Utils/ThemeManager.php
+++ application/Espo/Core/Utils/ThemeManager.php
@@ -35,7 +35,7 @@ class ThemeManager
 {
     private string $defaultName = 'Espo';
 
-    private string $defaultLogoSrc = 'client/img/logo.svg';
+    private string $defaultLogoSrc = 'client/img/zerosoftlogo.svg';
 
     public function __construct(
         private Config $config,
